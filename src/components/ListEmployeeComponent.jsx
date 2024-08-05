cfe33a2bfc9b0da6391d24031ed379aee33d3877
Sepide Jamshididana <s86908@bht-berlin.de>
2024-08-05 12:10:56 +0200

    added services and axios to connect with rest api

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import listEmployees from "../services/EmployeeService";
 
 const ListEmployeeComponent = () => {
-  const dummyData = [
-    {
-      id: 1,
-      firstName: "John",
-      lastName: "Dana",
-      email: "john@gmail.com",
-    },
-    {
-      id: 2,
-      firstName: "Sepid",
-      lastName: "Jd",
-      email: "sep@gmail.com",
-    },
-    {
-      id: 3,
-      firstName: "Hoda",
-      lastName: "hadi",
-      email: "hadi@gmail.com",
-    },
-  ];
+  const [employees, setEmployee] = useState([]);
 
-  return <div>ListEmployeeComponent</div>;
+  useEffect(() => {
+    listEmployees()
+      .then((response) => {
+        setEmployee(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="text-center">List of Employee</h2>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Employee Id </th>
+            <th>Employee First Name </th>
+            <th>Employee Last Name </th>
+            <th>Employee Email </th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ListEmployeeComponent;
