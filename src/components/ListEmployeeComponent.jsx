8fe784fd11e437ed9f4a8cd3c999b19404843686
Sepide Jamshididana <s86908@bht-berlin.de>
2024-08-05 11:09:12 +0200

    added bootstrap

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ListEmployeeComponent = () => {
+  const dummyData = [
+    {
+      id: 1,
+      firstName: "John",
+      lastName: "Dana",
+      email: "john@gmail.com",
+    },
+    {
+      id: 2,
+      firstName: "Sepid",
+      lastName: "Jd",
+      email: "sep@gmail.com",
+    },
+    {
+      id: 3,
+      firstName: "Hoda",
+      lastName: "hadi",
+      email: "hadi@gmail.com",
+    },
+  ];
+
+  return <div>ListEmployeeComponent</div>;
+};
+
+export default ListEmployeeComponent;
