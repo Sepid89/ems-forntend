e0ea56077c86a38a002aa25f4c71580fe343f512
Sepide Jamshididana <s86908@bht-berlin.de>
2024-08-06 09:59:42 +0200

    added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
 function App() {
   return (
-    <>
-      <ListEmployeeComponent />
-    </>
+    <div id="root" className="d-flex flex-column min-vh-100">
+      <HeaderComponent />
+      <main className="flex-grow-1">
+        <ListEmployeeComponent />
+      </main>
+      <FooterComponent></FooterComponent>
+    </div>
   );
 }
 export default App;
