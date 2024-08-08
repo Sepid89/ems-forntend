ff9eece54fae7caf0c8ba2a328d0ecfcdab87941
Sepide Jamshididana <s86908@bht-berlin.de>
2024-08-08 18:15:15 +0200

    added get emp by id to service

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -3,10 +3,13 @@ import axios from "axios";
 const REST_API_BASE_URL = "http://localhost:8080/api/employees";
 const listEmployees = () => axios.get(REST_API_BASE_URL);
 const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
+const getEmployee = (employeeId) =>
+  axios.get(REST_API_BASE_URL + "/" + employeeId);
 
 const EmployeeService = {
   listEmployees,
   createEmployee,
+  getEmployee,
 };
 
 export default EmployeeService;
